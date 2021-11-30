e864c4fe6552cf5ae4e1c0c43745039fdd82a9e4
Salman Mughal <m.salman@code-avenue.com>
2021-11-30 19:00:39 +0500

    feat: Company dashobard screen

---
+++ src/containers/CompanyDashboard/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class CompanyDashboard extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <>
+        <h1>Company Dashboard</h1>
+      </>
+    );
+  }
+}
+
+export default CompanyDashboard
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,9 +1,11 @@
 import Login from "./Login";
 import Signup from "./Signup";
-import StudentDashboard from "./StudentDashboard"
+import StudentDashboard from "./StudentDashboard";
+import CompanyDashboard from "./CompanyDashboard";
 
 export {
     Login,
     Signup,
-    StudentDashboard
+    StudentDashboard,
+    CompanyDashboard
 }
